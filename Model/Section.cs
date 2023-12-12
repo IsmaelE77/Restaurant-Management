0f3f6679887a1163145fc05b28a5993205fcdedb
omaralian98 <omaralian9870@gmail.com>
2023-12-12 18:22:58 +0300

    After finishing Category & Secton

--- Model/Section.cs
+++ Model/Section.cs
@@ -7,4 +7,5 @@ public class Section(int? _Id = null, string? _Name = "")
     [Required(ErrorMessage = "{0} is required")]
     [StringLength(100, MinimumLength = 1, ErrorMessage = "Length of Section shouldn't exceed 100")]
     public string Name { get; set; } = _Name ?? string.Empty;
+    public override string ToString() => $"Section(Id: {Id}, Name: {Name})";
 }
--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -1,64 +1,64 @@
 ﻿namespace Restaurant_Management.Repository;
 
-public class SectionRepository(string connectionString) : ISection
+public class CategoryRepository(string connectionString) : ICategory
 {
-    public int Add(Section section)
+    public int Add(Category category)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"insert into \"Section\"(Name) Values('{section.Name}')", con);
+        using var cmd = new OracleCommand($"insert into \"Category\"(Name) Values('{category.Name}')", con);
         return cmd.ExecuteNonQuery();
     }
     public int? GetId(string name)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"Select Id from \"Section\" where Name='{name}'", con);
+        using var cmd = new OracleCommand($"Select Id from \"Category\" where Name='{name}'", con);
         return Convert.ToInt32(cmd.ExecuteScalar());
     }
-    public Section? Get(int Id)
+    public Category? Get(int Id)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"Select Name from \"Section\" where Id='{Id}'", con);
+        using var cmd = new OracleCommand($"Select Name from \"Category\" where Id='{Id}'", con);
         return new(Id, cmd.ExecuteScalar()?.ToString());
     }
 
-    public IEnumerable<Section> GetAll()
+    public IEnumerable<Category> GetAll()
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand("Select * from \"Section\"", con);
+        using var cmd = new OracleCommand("Select * from \"Category\"", con);
         using var reader = cmd.ExecuteReader();
 
         while (reader.Read())
         {
             int id = reader.GetInt32(reader.GetOrdinal("Id"));
             string name = reader.GetString(reader.GetOrdinal("Name"));
-            yield return new Section(id, name);
+            yield return new Category(id, name);
         }
     }
     public int RemoveById(int Id)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"Delete from \"Section\" where Id='{Id}'", con);
+        using var cmd = new OracleCommand($"Delete from \"Category\" where Id='{Id}'", con);
         return cmd.ExecuteNonQuery();
     }
 
-    public int Remove(Section section)
+    public int Remove(Category category)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"Delete from \"Section\" where Id='{section.Id}' and Name='{section.Name}'", con);
+        using var cmd = new OracleCommand($"Delete from \"Category\" where Id='{category.Id}' and Name='{category.Name}'", con);
         return cmd.ExecuteNonQuery();
     }
 
-    public int Update(Section section)
+    public int Update(Category category)
     {
         using var con = new OracleConnection(connectionString);
         con.Open();
-        using var cmd = new OracleCommand($"Update \"Section\" Set Name='{section.Name}' where Id='{section.Id}'", con);
+        using var cmd = new OracleCommand($"Update \"Category\" Set Name='{category.Name}' where Id='{category.Id}'", con);
         return cmd.ExecuteNonQuery();
     }
 }
