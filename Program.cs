3d5900d6a2c7a89320b00681d2d6232ca3e1d428
omaralian98 <omaralian9870@gmail.com>
2023-12-18 15:44:21 +0300

    Some Chanages

--- Program.cs
+++ Program.cs
@@ -7,30 +7,31 @@ public class Program
     {
         return new OracleConnection(CONNECTION_STRING);
     }
+    public static void SeedData()
+    {
+        DataSeeder dataSeeder = new(GetIEmployee(), GetIEmployee_WorkDay(), GetISection(), GetIItem(),
+            GetIOrder(), GetIOrder_Item(), GetITable(), GetICategory(), GetISupplier(),
+            GetIIngredient(), GetIItem_Ingredient(), GetISupplier_Ingredient(), GetIReceipt());
+        //Seed the tables with initial data
+        dataSeeder.SeedData();
+    }
+    public static ICategory GetICategory() => new CategoryRepository(CONNECTION_STRING);
+    public static IEmployee_WorkDay GetIEmployee_WorkDay() => new Employee_WorkDayRepository(CONNECTION_STRING);
+    public static ISection GetISection() => new SectionRepository(CONNECTION_STRING);
+    public static IOrder_Item GetIOrder_Item() => new Order_ItemRepository(CONNECTION_STRING);
+    public static ITable GetITable() => new TableRepository(CONNECTION_STRING);
+    public static IItem_Ingredient GetIItem_Ingredient() => new Item_IngredientRepository(CONNECTION_STRING);
+    public static ISupplier_Ingredient GetISupplier_Ingredient() => new Supplier_IngredientRepository(CONNECTION_STRING);
+    public static IEmployee GetIEmployee() => new EmployeeRepository(CONNECTION_STRING, GetIEmployee_WorkDay());
+    public static IItem GetIItem() => new ItemRepository(CONNECTION_STRING, GetIItem_Ingredient(), GetIOrder_Item());
+    public static IOrder GetIOrder() => new OrderRepository(CONNECTION_STRING, GetIOrder_Item());
+    public static ISupplier GetISupplier() => new SupplierRepository(CONNECTION_STRING, GetISupplier_Ingredient());
+    public static IIngredient GetIIngredient() => new IngredientRepository(CONNECTION_STRING, GetISupplier_Ingredient(), GetIItem_Ingredient());
+    public static IReceipt GetIReceipt() => new ReceiptRepository(CONNECTION_STRING, GetIOrder());
+
 
     public static void Main()
     {
-        //Create Tables
         Database.CreateTables(GetConnection());
-        //Implement the interfaces
-        IEmployee_WorkDay employee_WorkDay = new Employee_WorkDayRepository(CONNECTION_STRING);
-        ISection sectionRepository = new SectionRepository(CONNECTION_STRING);
-        IOrder_Item orderItemRepository = new Order_ItemRepository(CONNECTION_STRING);
-        ITable tableRepository = new TableRepository(CONNECTION_STRING);
-        ICategory categoryRepository = new CategoryRepository(CONNECTION_STRING);
-        IItem_Ingredient itemIngredientRepository = new Item_IngredientRepository(CONNECTION_STRING);
-        ISupplier_Ingredient supplierIngredientRepository = new Supplier_IngredientRepository(CONNECTION_STRING);
-        IEmployee employeeRepository = new EmployeeRepository(CONNECTION_STRING, employee_WorkDay);
-        IItem itemRepository = new ItemRepository(CONNECTION_STRING, itemIngredientRepository, orderItemRepository);
-        IOrder orderRepository = new OrderRepository(CONNECTION_STRING, orderItemRepository);
-        ISupplier supplierRepository = new SupplierRepository(CONNECTION_STRING, supplierIngredientRepository);
-        IIngredient ingredientRepository = new IngredientRepository(CONNECTION_STRING, supplierIngredientRepository, itemIngredientRepository);
-        IReceipt receipt = new ReceiptRepository(CONNECTION_STRING, orderRepository);
-        //Pass the interfaces to the DateSeeder Class 
-        DataSeeder dataSeeder = new(employeeRepository, employee_WorkDay, sectionRepository, itemRepository,
-            orderRepository, orderItemRepository, tableRepository, categoryRepository, supplierRepository,
-            ingredientRepository, itemIngredientRepository, supplierIngredientRepository, receipt);
-        //Seed the tables with initial data
-        dataSeeder.SeedData();
     }
 }
